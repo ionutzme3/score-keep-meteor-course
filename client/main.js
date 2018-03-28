460a1508c0dd911de05c980f63f6861c80373d07
Croitoru Ion <ionutzme3@gmail.com>
2018-03-28 15:43:00 +0300

    init commit

---
+++ client/main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+
+import {Players, calculatePlayerPosition} from './../imports/api/players';
+import App from './../imports/ui/App';
+
+
+
+
+Meteor.startup(() =>{
+Tracker.autorun(() =>{
+  let players = Players.find({}, {sort: {score: -1}}).fetch();
+  let positionPlayer = calculatePlayerPosition(players);
+  let title = 'Score Keep';
+  ReactDom.render(<App title={title} players={positionPlayer}/>, document.getElementById('app'));
+});
+
+});
